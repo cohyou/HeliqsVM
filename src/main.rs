eda93e57fc88392cc8b1ddf40344ec751c2e5c75
OKAZAKI Yasuo <yasuo_okazaki@fandmnet.com>
2016-08-10 18:25:50 +0900

    try using read_line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
     println!("wowowow");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("!!!! FAIL READ !!!!");
+
+    println!("you typed {}", guess);
 }
