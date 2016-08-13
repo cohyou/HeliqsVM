ef80a870b2015046c3595081584053beccbf745d
cohyou <cohyou@aurola.net>
2016-08-13 17:16:53 +0900

    start HeliqsVM repl in rust

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 use std::io;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+struct Briq {
+    l: [i8; 8],
+    h: [i8; 8],
+}
+
+impl Briq {
+    fn new() -> Briq {
+        Briq { l: [0; 8], h: [0; 8] }
+    }
+}
+
+fn show(m: &HashMap<i16, Vec<Briq>>) {
+    for (i, v) in m {
+        println!("{}", i);
+    }
+    // println!("{}", m);
+}
 
 fn main() {
-    println!("wowowow");
+    let mut m: HashMap<i16, Vec<Briq>> = HashMap::new();
 
-    let mut guess = String::new();
+    let b = Briq::new();
 
-    io::stdin().read_line(&mut guess).expect("!!!! FAIL READ !!!!");
+    m.insert(-1, vec![b]);
 
-    println!("you typed {}", guess);
+    loop {
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("!!!! FAIL READ !!!!");
+
+        if guess == "quit\n" {
+            println!("bye!");
+            break;
+        } else {
+            println!("you typed {}", guess);
+            show(&m);
+        }
+    }
 }
